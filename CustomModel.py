7c4a470e3eb90ecb3a212026161b535435e2b29d
DonghwanKim <66504341+SimplePro@users.noreply.github.com>
2021-01-20 10:58:32 +0900

    Add files via upload

--- CustomModel.py
+++ CustomModel.py
@@ -2,25 +2,52 @@ from keras.models import Sequential, load_model
 import numpy as np
 import json
 
+from keras.layers import *
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+
 
 class CustomCnn:
 
-    def __init__(self, generator=True, image_shape=None, batch_size=None, model_name=None):
-        if image_shape is None:
-            raise Exception("image_shape must not be None")
-        if batch_size is None:
-            raise Exception("batch_size must not be None")
+    def __init__(self, generator=False, model_name=None):
         if model_name is None:
             raise Exception("model_name must not be None")
         self.generator = generator
-        self.image_shape = image_shape
-        self.batch_size = batch_size
         self.model_name = model_name
         self.class_indices = {}
 
-    def _modeling(self, input_shape=None, model_info=None):
+    def _auto_modeling(self, input_shape=None, class_n=None):
         if input_shape is None:
             raise Exception("input_shape must not be None")
+        if class_n is None:
+            raise Exception("class_n must not be None")
+        self.model = Sequential()
+        self.model.add(Conv2D(64, kernel_size=(3, 3), padding='same', input_shape=input_shape, activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.25))
+
+        self.model.add(Conv2D(128, kernel_size=(3, 3), padding='same', activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.25))
+
+        self.model.add(Conv2D(256, kernel_size=(3, 3), padding='same', activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.25))
+
+        self.model.add(Conv2D(512, kernel_size=(3, 3), padding='same', activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.25))
+
+        self.model.add(Flatten())
+
+        self.model.add(Dense(512, activation="relu"))
+        self.model.add(Dropout(0.25))
+
+        self.model.add(Dense(256, activation='relu'))
+        self.model.add(Dropout(0.25))
+
+        self.model.add(Dense(class_n, activation='softmax'))
+
+    def _modeling(self, model_info=None):
         if model_info is None:
             raise Exception("model_info must not be None")
 
@@ -29,7 +56,7 @@ class CustomCnn:
         for mi in model_info:
             self.model.add(mi)
 
-    def _model_compiling(self, loss=None, optimizer=None, metrics=None):
+    def _compiling(self, loss=None, optimizer=None, metrics=None):
         if loss is None:
             raise Exception("loss must not be None")
         if optimizer is None:
@@ -44,8 +71,15 @@ class CustomCnn:
     def _summary(self):
         return self.model.summary()
 
-    def _fit(self, train_data_generator, test_data_generator, train_directory, test_directory, train_set, test_set,
-             epochs=None, steps_per_epoch=None, validation_steps=None, batch_size=None, verbose=None):
+    def _fit(self, train_data_generator=None, test_data_generator=None, train_directory=None, test_directory=None,
+             train_set=None, test_set=None, image_shape=None, epochs=None, steps_per_epoch=None, validation_steps=None,
+             batch_size=None, verbose=None):
+        if batch_size is None:
+            raise Exception("batch_size must not be None")
+
+        self.image_shape = image_shape
+        self.batch_size = batch_size
+
         if self.generator:
             if train_data_generator is None:
                 raise Exception("train_data_generator must not be None")
@@ -57,6 +91,8 @@ class CustomCnn:
                 raise Exception("epochs must not be None")
             if validation_steps is None:
                 raise Exception("validation_steps must not be None")
+            if image_shape is None:
+                raise Exception("image_shape must not be None")
 
             self.train_set = train_data_generator.flow_from_directory(
                 train_directory,
@@ -119,11 +155,11 @@ class CustomCnn:
     def _save_model(self, directory=None):
         if directory is None:
             raise Exception("directory must not be None")
-        self.model.save(directory + "/" + self.model_name + ".h5")
+        self.model.save(directory + self.model_name + ".h5")
 
         data = {"generator": self.generator, "image_shape": self.image_shape, "batch_size": self.batch_size,
                 "model_name": self.model_name, "class_indices": self.class_indices,
-                "model": directory + "/" + self.model_name + ".h5"}
+                "model": directory + self.model_name + ".h5"}
         try:
             data["train_set"] = self.train_set
             data["test_set"] = self.test_set
@@ -131,15 +167,15 @@ class CustomCnn:
             pass
 
         try:
-            data["x_train"] = self.x_train
-            data["y_train"] = self.y_train
-            data["x_test"] = self.x_test
-            data["y_test"] = self.y_test
+            data["x_train"] = self.x_train.tolist()
+            data["y_train"] = self.y_train.tolist()
+            data["x_test"] = self.x_test.tolist()
+            data["y_test"] = self.y_test.tolist()
         except:
             pass
 
-        with open(directory + "/" + self.model_name + ".json", "w") as f:
-            json.dump(data, f, indent=4)
+        with open(directory + self.model_name + ".json", "w") as f:
+            json.dump(data, f)
 
     def _load_model(self, directory=None, model_name=None):
         if directory is None:
@@ -147,7 +183,7 @@ class CustomCnn:
         if model_name is None:
             raise Exception("model_name must not be None")
 
-        with open(directory + "/" + self.model_name + ".json", "r") as f:
+        with open(directory + model_name + ".json", "r") as f:
             json_data = json.load(f)
             self.generator = json_data["generator"]
             self.image_shape = json_data["image_shape"]
