1df0a074c31148a76b2c9b89aa97d4b3d287cb63
DonghwanKim <66504341+SimplePro@users.noreply.github.com>
2021-01-22 14:59:22 +0900

    added Preprocessing module file

--- Preprocessing.py
+++ Preprocessing.py
@@ -5,6 +5,11 @@ import os
 
 from keras_preprocessing.image import ImageDataGenerator
 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from urllib.request import urlretrieve
+
 
 class ImagePreprocess:
 
@@ -51,3 +56,43 @@ class ImagePreprocess:
                 img = cv2.imread(directory + file)
                 img = cv2.resize(img, dsize=size)
                 cv2.imwrite(directory + name_list[i], img)
+
+    def search(self, search_name=None, directory=None):
+        if search_name is None:
+            raise Exception("search_name must not be None")
+        if directory is None:
+            raise Exception("directory must not be None")
+
+        driver = webdriver.Chrome()
+        driver.get("https://www.google.co.kr/imghp?hl=ko&tab=wi&ogbl")
+        elem = driver.find_element_by_name("q")
+        elem.send_keys(search_name)
+        elem.send_keys(Keys.RETURN)
+
+        last_height = driver.execute_script("return document.body.scrollHeight")
+
+        while True:
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(1)
+            new_height = driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                try:
+                    driver.find_element_by_css_Selector(".mye4qd").click()
+                except:
+                    break
+            last_height = new_height
+
+        images = driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
+        count = 1
+        for image in images:
+            try:
+                image.click()
+                time.sleep(0.2)
+                imgURL = driver.find_element_by_xpath(
+                    '//*[@id="Sva75c"]/div/div/div[3]/div[2]/c-wiz/div[1]/div[1]/div/div[2]/a/img').get_attribute("src")
+                urlretrieve(imgURL, directory + search_name + str(count) + ".jpg")
+                count += 1
+            except:
+                pass
+
+        driver.close()
